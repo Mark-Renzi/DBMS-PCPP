5d01f254d916b9b32b7a0a4a0b3e1b3c12f0efff
mrenzi402 <marenzi@wpi.edu>
2023-10-02 17:31:37 -0400

    removed bonus data

--- python/cleaner.py
+++ python/cleaner.py
@@ -4,7 +4,16 @@ import os
 import sys
 import re
 import numpy as np
+import uuid
 
-cpu = pd.read_json('data/json/cpu.json')
+# create an empty dataframe to hold generic part info,
+# aggregated from all tables
+part = pd.DataFrame(columns=['id', 'price', 'manufacturer', 'model'])
 
+cpu = pd.read_json('data/pcpartpicker/cpu.json')
+cpu['id'] = [uuid.uuid4() for _ in range(len(cpu.index))]
 print(cpu.head())
+
+gpu = pd.read_json('data/pcpartpicker/video-card.json')
+gpu['id'] = [uuid.uuid4() for _ in range(len(gpu.index))]
+print(gpu.head())
