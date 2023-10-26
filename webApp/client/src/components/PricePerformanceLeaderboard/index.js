d7075e38d99eea3ce27fd8eb0b0695fd2f227279
mrenzi402 <marenzi@wpi.edu>
2023-10-26 18:38:18 -0400

    node proxy compatibility fix

--- webApp/client/src/components/PricePerformanceLeaderboard/index.js
+++ webApp/client/src/components/PricePerformanceLeaderboard/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Pagination from 'react-bootstrap/Pagination';
 import Modal from 'react-bootstrap/Modal';
