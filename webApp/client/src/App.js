f8e0e7f091756071753a81de5e1d7c42290dd7ec
MarekSGarbaczonek <msgarbaczonek@gmail.com>
2023-10-28 07:30:00 -0400

    Home Page Stuff

--- webApp/client/src/App.js
+++ webApp/client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
+import HomePage from './components/Home';
 import Header from './components/Header';
 import Lists from './components/Lists';
 import PricePerformanceLeaderboard from './components/PricePerformanceLeaderboard';
@@ -19,7 +19,7 @@ class App extends Component {
 					<div className="container">
 						<div className="page-container">
 							<Routes>
-								<Route exact path="/" element={<Home />} />
+								<Route exact path="/" element={<HomePage />} />
 								<Route path="/leaderboard" element={<PricePerformanceLeaderboard />} />
 								<Route path="/lists" element={<Lists />} />
 							</Routes>
