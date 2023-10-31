d9d8ba17a1667a86f4bb961f424c0aa7598c258b
mrenzi402 <marenzi@wpi.edu>
2023-10-31 16:36:06 -0400

    cards flexbox

--- webApp/client/src/components/Home/index.js
+++ webApp/client/src/components/Home/index.js
@@ -80,60 +80,52 @@ const HomePage = () => {
 
 	return (
 		<div className="HomeContainer">
+
+			<Modal show={showListModal} onHide={handleModalClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Create New List</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div>
+						<label>Name</label>
+						<input
+							id='name'
+							type='text'
+							onChange={(e) => setNewListName(e.target.value)}
+							value={newListName}
+							className="description-box"
+						/>
+					</div>
+					<div>
+						<label>Description</label>
+						<input
+							id='description'
+							type='text'
+							onChange={(e) => setNewListDescription(e.target.value)}
+							value={newListDescription}
+							className="description-box"
+						/>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleModalClose}>Cancel</Button>
+					<Button variant="primary" onClick={handleListCreation}>Make List</Button>
+				</Modal.Footer>
+			</Modal>
+
 			<div className="ListsContainer">
-				<Grid container spacing={2}>
-					<Grid item key={0} xs={6} sm={4} md={4} lg={3}>
-						<div className="card-c">
-							<Modal show={showListModal} onHide={handleModalClose}>
-								<Modal.Header closeButton>
-									<Modal.Title>Create New List</Modal.Title>
-								</Modal.Header>
-								<Modal.Body>
-									<div>
-										<label>Name</label>
-										<input
-											id='name'
-											type='text'
-											onChange={(e) => setNewListName(e.target.value)}
-											value={newListName}
-											className="description-box"
-										/>
-									</div>
-									<div>
-										<label>Description</label>
-										<input
-											id='description'
-											type='text'
-											onChange={(e) => setNewListDescription(e.target.value)}
-											value={newListDescription}
-											className="description-box"
-										/>
-									</div>
-								</Modal.Body>
-								<Modal.Footer>
-									<Button variant="secondary" onClick={handleModalClose}>Cancel</Button>
-									<Button variant="primary" onClick={handleListCreation}>Make List</Button>
-								</Modal.Footer>
-							</Modal>
-							<button className='card-button' onClick={handleNewListClick}>
-								<p className="card-text card-new text-center">+</p>
-							</button>
-						</div>
-					</Grid>
-					<Grid item xs={6} sm={4} md={4} lg={3}>
-						{ userLists.map((partlist) => (
-							<div key={partlist._id} className="card-c grid-item">
-								<button className='card-button'>
-									<p className="card-text text-center card-tit">{partlist.name}</p>
-									<hr className='hr-line' />
-									<p className="card-text text-center card-desc">{partlist.description}</p>
-									<hr className='hr-line' />
-									<p className="card-text text-center card-price">${partlist.price}</p>
-								</button>
-						</div>))}
-					</Grid>
-					
-				</Grid>
+				<button className='card-button' onClick={handleNewListClick} title='Create a new list'>
+					<p className="card-text card-new text-center">+</p>
+				</button>
+				{ userLists.map((partlist) => (
+					<button className='card-button'>
+						<p className="card-text text-center card-tit">{partlist.name}</p>
+						<hr className='hr-line' />
+						<p className="card-text text-center card-desc">{partlist.description}</p>
+						<hr className='hr-line' />
+						<p className="card-text text-center card-price">${partlist.price}</p>
+					</button>
+				))}
 			</div>
 		</div>
 	)
