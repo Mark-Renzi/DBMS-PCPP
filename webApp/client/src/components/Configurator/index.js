cc104cabbd03c73f44790aad5ed88e27148dea3c
mrenzi402 <marenzi@wpi.edu>
2023-11-04 12:30:51 -0400

    configurator loading symbol

--- webApp/client/src/components/Configurator/index.js
+++ webApp/client/src/components/Configurator/index.js
@@ -2,73 +2,77 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style.css';
 import { useParams } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Configurator = () => {
-  const componentNames = ["CPU", "CPU Cooler", "Motherboard", "Memory", "Graphics Card", "Storage", "Case", "Power Supply"];
-  const [parts, setParts] = useState([]);
-  const { listid } = useParams();
+    const componentNames = ["CPU", "CPU Cooler", "Motherboard", "Memory", "Graphics Card", "Storage", "Case", "Power Supply"];
+    const [listLoading, setListLoading] = useState(false);
+    const [parts, setParts] = useState(
+        componentNames.map(name => ({
+            name,
+            model: null,
+            manufacturer: null,
+            price: null,
+        }))
+    );
+    const { listid } = useParams();
 
-  useEffect(() => {
-    getPartsList();
-  }, []);
+    useEffect(() => {
+        getPartsList();
+    }, []);
 
-  const addComponent = (index) => {
-    window.location.href = `http://localhost:3000/browse/${index}`;
-  };
+    const addComponent = (index) => {
+        window.location.href = `http://localhost:3000/browse/${index}`;
+    };
 
-  const getPartsList = () => {
-    axios.get(`/api/configurator/${listid}`)
-      .then(response => {
-        const partsList = componentNames.map((componentName, index) => {
-          if (Object.keys(response.data[index]).length !== 0) {
-            const part = response.data[index];
-            return (
-              <tr key={index}>
-                <td>{componentName}</td>
-                <td>{part.model}</td>
-                <td>{part.manufacturer}</td>
-                <td>${part.price}</td>
-              </tr>
-            );
-          } 
-          else {
-            return (
-              <tr key={index}>
-                <td>{componentName}</td>
-                <td>
-                  <button onClick={() => addComponent(index)}> + Choose {componentName}</button>
-                </td>
-                <td></td>
-                <td></td>
-              </tr>
-            );
-          }
-        });
-        setParts(partsList);
-      })
-      .catch(error => {
-        console.error('Error fetching user list!', error);
-      });
-  }
+    const getPartsList = () => {
+        setListLoading(true);
+        axios.get(`/api/configurator/${listid}`)
+            .then(response => {
+                const newParts = componentNames.map((componentName, index) => {
+                    const hasPartData = response.data[index] && Object.keys(response.data[index]).length !== 0;
+                    return hasPartData ? response.data[index] : { name: componentName };
+                });
+                setParts(newParts);
+                setListLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching parts list!', error);
+                setListLoading(false);
+            });
+    }
 
-  return (
-    <>
-      <h1>Configurator</h1>
-      <table id="part-table">
-        <thead>
-          <tr>
-            <th>Component</th>
-            <th>Name</th>
-            <th>Manufacturer</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {parts}
-        </tbody>
-      </table>
-    </>
-  )
+    const renderRow = (part, index) => {
+        return (
+            <tr key={index}>
+                <td>{part.name}</td>
+                <td>{part.model || (listLoading ? <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner> : <button onClick={() => addComponent(index)}> + Choose {part.name}</button>)}</td>
+                <td>{part.manufacturer || ''}</td>
+                <td>{part.price ? `$${part.price}` : ''}</td>
+            </tr>
+        );
+    };
+
+    return (
+        <>
+            <h1>Configurator</h1>
+            <table id="part-table">
+                <thead>
+                    <tr>
+                        <th>Component</th>
+                        <th>Name</th>
+                        <th>Manufacturer</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {parts.map(renderRow)}
+                </tbody>
+            </table>
+        </>
+    )
 };
 
 export default Configurator;
