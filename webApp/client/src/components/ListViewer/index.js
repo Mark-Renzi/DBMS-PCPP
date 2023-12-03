39bcd160fa7ad519dd9ba37eb9f65ad8e445dce5
mrenzi402 <marenzi@wpi.edu>
2023-12-02 22:07:01 -0500

    added loading symbols to list viewer -- removed redundant route -- removed DDOS generator

--- webApp/client/src/components/ListViewer/index.js
+++ webApp/client/src/components/ListViewer/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 import PageTitleContext from '../../context/pageTitleContext';
 
 const ListViewer = () => {
@@ -15,24 +16,25 @@ const ListViewer = () => {
   );
     const [listInfo, setListInfo] = useState({});
     const [listTDP, setListTDP] = useState({});
+    const [configuratorLoading, setcConfiguratorLoading] = useState(false);
+    const [listInfoLoading, setListInfoLoading] = useState(false);
+    const [listTDPLoading, setListTDPLoading] = useState(false);
+
     const { listid } = useParams();
 
     const { updatePageTitle } = useContext(PageTitleContext);
 
     useEffect(() => {
-      getPartsList();
-      getListInfo();
-      getListTDP();
-      updatePageTitle("Part List");
-  }, []);
+        getPartsList();
+        getListInfo();
+        getListTDP();
+        updatePageTitle("Part List");
+    }, []);
 
-  useEffect(() => {
-    getListInfo();
-  }, [listInfo])
-
-  const getPartsList = () => {
-    axios.get(`/api/publicbuild/${listid}`)
-        .then(response => {
+    const getPartsList = async () => {
+        setcConfiguratorLoading(true);
+        try {
+            let response = await axios.get(`/api/configurator/${listid}`);
             const newParts = componentNames.map(() => ({
                 name: '',
                 model: null,
@@ -59,27 +61,30 @@ const ListViewer = () => {
             });
 
             setParts(newParts);
-        })
-        .catch(error => {
-            console.error('Error fetching parts list!', error);
-        });
-      };
+            setcConfiguratorLoading(false);
+        } catch (e) {
+            console.log("Error fetching parts list:", e);
+        }
+    };
 
-    const getListInfo = () => {
-        axios.get(`/api/listinfo/${listid}`)
-            .then(response => {
-                setListInfo(response.data);
-                updatePageTitle("List: " + listInfo.name);
-            })
-            .catch(error => {
-                console.error('Error fetching list info!', error);
-            });
+    const getListInfo = async () => {
+        setListInfoLoading(true);
+        try {
+            let response = await axios.get(`/api/listinfo/${listid}`);
+            setListInfo(response.data);
+            updatePageTitle("List: " + response?.data.name);
+            setListInfoLoading(false);
+        } catch (e) {
+            console.log("Error fetching list info:", e);
+        }
     };
 
     const getListTDP = () => {
+        setListTDPLoading(true);
         axios.get(`/api/listtdp/${listid}`)
             .then(response => {
                 setListTDP(response.data);
+                setListTDPLoading(false);
             })
             .catch(error => {
                 console.error('Error fetching list TDP!', error);
@@ -89,38 +94,57 @@ const ListViewer = () => {
     return (
         <>
             <div className='Page-Content-Container'>
-                <table className='Table-Base'>
-                    <thead>
-                        <tr>
-                            <th>Component</th>
-                            <th>Name</th>
-                            <th>Manufacturer</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {parts.map((part, index) => (
-                            <tr key={index} className='Table-Base-TR'>
-                                <td id='TD-Start'><strong>{part.name}</strong></td>
-                                <td>{part.model}</td>
-                                <td>{part.manufacturer}</td>
-                                <td id='TD-End'>{part.price}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                { configuratorLoading ? 
+                    (
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    ) : (
+                        <table className='Table-Base'>
+                            <thead>
+                                <tr>
+                                    <th>Component</th>
+                                    <th>Name</th>
+                                    <th>Manufacturer</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {parts.map((part, index) => (
+                                    <tr key={index} className='Table-Base-TR'>
+                                        <td id='TD-Start'><strong>{part.name}</strong></td>
+                                        <td>{part.model}</td>
+                                        <td>{part.manufacturer}</td>
+                                        <td id='TD-End'>{part.price}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )
+                }
+                
             </div>
             <div className='Page-Content-Container Spacer'>
                  <div className='Bottom-Info-Container'>
-                    <div className='Bottom-Info'>
-                        <span>Price: ${listInfo.totalprice}</span>
-                        <span>TDP: {listTDP.sum_tdp} W</span>
-                    </div>
-                    {listInfo.description !== '' ? (
-                            <span className='Info-Desc'>{listInfo.description}</span>
+                    { listInfoLoading || listTDPLoading ?
+                        (
+                            <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
                         ) : (
-                            null
-                    )}
+                            <>
+                                <div className='Bottom-Info'>
+                                    <span>Price: ${listInfo.totalprice}</span>
+                                    <span>TDP: {listTDP.sum_tdp} W</span>
+                                </div>
+                                {listInfo.description !== '' ? (
+                                        <span className='Info-Desc'>{listInfo?.description}</span>
+                                    ) : (
+                                        null
+                                )}
+                            </>
+                        )
+                    }
                 </div>
             </div>
         </>
