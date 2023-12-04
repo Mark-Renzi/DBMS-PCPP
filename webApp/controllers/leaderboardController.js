dfecb18e7d106b00daa30a621cb4f51bd70ef9ed
mrenzi402 <marenzi@wpi.edu>
2023-12-04 15:54:32 -0500

    leaderboard list scores

--- webApp/controllers/leaderboardController.js
+++ webApp/controllers/leaderboardController.js
@@ -53,67 +53,40 @@ const getListScores = async (req, res, db) => {
   
   let lists = await db.query(`
     SELECT
-        userid,
-        totalprice,
-        name,
-        description,
-        listid,
-        SUM(score) AS listscore
-    FROM (
-        SELECT
-            pl.userid,
-            pl.totalprice,
-            pl.name,
-            pl.description,
-            lc.listid,
-            benchmark.score
-        FROM
-            listcontains lc
-            JOIN computerpart cp ON lc.partid = cp.partid
-            JOIN partslist pl ON lc.listid = pl.listid
-            JOIN cpu ON cpu.partid = cp.partid
-            JOIN benchmark ON benchmark.chipsetid = cpu.chipsetid
-        WHERE
-            benchmark.type = $1
-        GROUP BY
-            pl.userid, pl.totalprice, pl.name, pl.description, lc.listid, cpu.chipsetid, benchmark.score
-
+        pl.userid,
+        pl.totalprice,
+        pl.name,
+        pl.description,
+        pl.listid,
+        COALESCE(SUM(benchmark.score), 0) AS listscore
+    FROM partslist pl
+    LEFT JOIN listcontains lc ON pl.listid = lc.listid
+    LEFT JOIN computerpart cp ON lc.partid = cp.partid
+    LEFT JOIN (
+        SELECT cpu.partid, b.score FROM cpu
+        JOIN benchmark b ON b.chipsetid = cpu.chipsetid AND b.type = $1
         UNION ALL
-
-        SELECT
-            pl.userid,
-            pl.totalprice,
-            pl.name,
-            pl.description,
-            lc.listid,
-            benchmark.score
-        FROM
-            listcontains lc
-            JOIN computerpart cp ON lc.partid = cp.partid
-            JOIN partslist pl ON lc.listid = pl.listid
-            JOIN gpu ON gpu.partid = cp.partid
-            JOIN benchmark ON benchmark.chipsetid = gpu.chipsetid
-        WHERE
-            benchmark.type = $2
-        GROUP BY
-            pl.userid, pl.totalprice, pl.name, pl.description, lc.listid, benchmark.score
-    ) AS scores
-    GROUP BY
-        userid, totalprice, name, description, listid
+        SELECT gpu.partid, b.score FROM gpu
+        JOIN benchmark b ON b.chipsetid = gpu.chipsetid AND b.type = $2
+    ) AS benchmark ON benchmark.partid = cp.partid
+    GROUP BY pl.userid, pl.totalprice, pl.name, pl.description, pl.listid
     ORDER BY listscore DESC
     OFFSET $3 LIMIT $4;`, [cpuBenchType, gpuBenchType, pageNumber, limitNumber]);
-    let listCount = await db.query(`
-        SELECT COUNT(*) FROM partslist;`);
-        
-        try {
-          return res.status(200).json({
-            lists: lists?.rows,
-            totalResultNum: listCount?.rows[0]?.count
-        });}catch (e) {
+
+  let listCount = await db.query(`
+    SELECT COUNT(*) FROM partslist;`);
+  
+  try {
+    return res.status(200).json({
+      lists: lists?.rows,
+      totalResultNum: listCount?.rows[0]?.count
+    });
+  } catch (e) {
     console.log(e);
     return res.status(500);
   }
 };
+
 
 module.exports = {
     getListTDPs,
